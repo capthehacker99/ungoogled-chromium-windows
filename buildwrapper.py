4a4b730872bf1d63c4fa918bd366caae95e73ca5
capthehacker99 <39180224+capthehacker99@users.noreply.github.com>
2022-09-03 17:44:17 +0700

    Update buildwrapper.py

--- buildwrapper.py
+++ buildwrapper.py
@@ -1,36 +1,13 @@
-from threading import Thread, Event
 import random
-import os
-from time import sleep
-# StoppableThread is from user Dolphin, from http://stackoverflow.com/questions/5849484/how-to-exit-a-multithreaded-program
-class StoppableThread(Thread):  
+from threading import Thread
+import time
 
-    def __init__(self):
-        Thread.__init__(self)
-        self.stop_event = Event()        
+def keepAliveFunction():
+    while True:
+        time.sleep(10)
+        print("KeepAlive: ", random.random())
 
-    def stop(self):
-        # set event to signal thread to terminate
-        self.stop_event.set()
-        # block calling thread until thread really has terminated
-        self.join()
-
-class IntervalTimer(StoppableThread):
-
-    def __init__(self, interval, worker_func):
-        super().__init__()
-        self._interval = interval
-        self._worker_func = worker_func
-
-    def run(self):
-        while not self.stop_event.is_set():
-            self._worker_func()
-            sleep(self._interval)
-
-def keepAlive():
-    print("KeepAlive: ", random.random())
-
-keepAliveInterval = IntervalTimer(10,keepAlive)
-keepAliveInterval.start()
+keepAliveThread = Thread(target = keepAliveFunction)
+keepAliveThread.daemon = True
+keepAliveThread.start()
 exec(open("build.py").read())
-keepAliveInterval.stop()
