3e35ad9de6595e2f92662d86345a2deaadb35c52
capthehacker99 <39180224+capthehacker99@users.noreply.github.com>
2022-09-03 22:04:39 +0700

    Update and rename buildwrapper.py to buildwrapper.bat

---
+++ buildwrapper.bat
@@ -0,0 +1,6 @@
+set PATH=C:\Windows\System32;C:\Windows;C:\Windows\System32\wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Windows\System32\OpenSSH\;C:\Program Files\PowerShell\7\;C:\Program Files\Google\Compute Engine\sysprep;C:\PROGRA~3\CHOCOL~1\bin;C:\PROGRA~1\Git\cmd;C:\PROGRA~1\Git\mingw64\bin;C:\PROGRA~1\Git\usr\bin;C:\Program Files\Git LFS;C:\Program Files\Amazon\AWSCLIV2\;C:\Program Files\Microsoft\Web Platform Installer\;C:\Program Files\Microsoft Service Fabric\bin\Fabric\Fabric.Code;C:\Program Files\Microsoft SDKs\Service Fabric\Tools\ServiceFabricLocalClusterManager;C:\Program Files\Go\bin;C:\PROGRA~1\nodejs\;C:\ProgramData\nvm;C:\PROGRA~1\nodejs;C:\Program Files\Docker;C:\PROGRA~1\dotnet\;C:\Program Files\Microsoft SQL Server\150\Tools\Binn\;C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\170\Tools\Binn\;C:\Program Files (x86)\IncrediBuild;C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin;C:\Python\Python310\;C:\Python\Python310\Scripts\
+set WINDOWSSDKDIR=C:\Program Files (x86)\Windows Kits\10
+set vs2022_install=C:\Program Files\Microsoft Visual Studio\2022\Community
+set GYP_MSVS_VERSION=2022
+py build.py
+py package.py
